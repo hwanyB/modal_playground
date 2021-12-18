8486111ef6e89d0eff330acf20a551e7a8722b21
hwanyb <mnn9502@naver.com>
2021-12-18 19:00:42 +0900

    styling

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -3,13 +3,29 @@ import styled from '@emotion/styled/macro';
 import Portal from './Portal';
 
 const Overlay = styled.div`
-
+position: fixed;
+z-index: 10;
+top: 0;
+right: 0;
+bottom: 0;
+left: 0;
+overflow: hidden;
+display: flex;
+align-items: center;
+justify-content: center;
 `;
 const Dim = styled.div`
-
+position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.5);
 `;
 const Container = styled.div`
-
+max-width: 456px;
+  position: relative;
+  width: 100%;
 `;
 
 interface Props {
@@ -20,12 +36,18 @@ interface Props {
 
 const Modal: React.FC<Props> = ({ children, isOpen, onClose, selector }) => {
     return (
-        <Portal selector={selector}>
+        <>
+            {
+                isOpen ? (
+                    <Portal selector={selector}>
         <Overlay>
             <Dim onClick={onClose} />
             <Container>{children}</Container>
         </Overlay>
         </Portal>
+                ): null
+            } 
+        </>
     )
 }
 
